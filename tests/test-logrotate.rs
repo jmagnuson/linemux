5c20c86e085e44c634ce3760047a06304959c790
Jon Magnuson <jon.magnuson@gmail.com>
2021-09-19 22:54:28 -0500

    fixup! nice works

--- tests/test-logrotate.rs
+++ tests/test-logrotate.rs
@@ -1,9 +1,9 @@
 use futures_util::stream::StreamExt;
-//use futures_util::FutureExt;
 use linemux::MuxedLines;
 use tempfile::tempdir;
 use tokio::process::Command;
 use tokio::time;
+//use std::process::Stdio;
 use std::time::Duration;
 
 #[tokio::test]
@@ -15,23 +15,18 @@ pub async fn test_logrotate() -> std::io::Result<()> {
     let logdir_path = logdir.path();
     let logfile = logdir_path.join("foo.log");
 
-    /*
-    let logdir_path_clone = logdir_path.to_owned();
-    let thandle = tokio::task::spawn_blocking(move || {
-        let _logdir_path = logdir_path_clone;
-    });
-    */
-
     // TODO: CARGO_MANIFEST_DIR mabye
-    //
+
     let mut child = Command::new("tests/test-logrotate.sh")
         .arg(logdir_path.to_str().unwrap())
         //.arg(line_vals_expected) // TODO: do csv or something)
+        //.stdout(Stdio::piped())
         .spawn()
         .expect("failed to spawn");
 
-    // Await until the command completes
-    //let status = child.wait().await?;
+    // TODO: pipe script stdout to logging
+    // let stdout = child.stdout.take().unwrap();
+    // let mut reader = BufReader::new(stdout).lines();
 
     let mut lines = MuxedLines::new()?;
 
@@ -42,12 +37,12 @@ pub async fn test_logrotate() -> std::io::Result<()> {
         .take(4)
         .collect::<Vec<_>>();
 
-    let (line_vals, _) = tokio::try_join!(
+    let (line_vals, status) = tokio::try_join!(
         time::timeout(Duration::from_millis(5000), line_vals_fut),
-        //time::timeout(Duration::from_millis(5000), thandle),
         time::timeout(Duration::from_millis(5000), child.wait()),
     ).unwrap();
 
+    assert!(status.unwrap().success());
     assert_eq!(line_vals_expected, line_vals);
 
     Ok(())
--- tests/test-logrotate.sh
+++ tests/test-logrotate.sh
@@ -8,10 +8,8 @@ set -ex
 # logrotate'd, to verify handling the rollover method of logrotate.
 #
 
-#logdir="$( mktemp -d )"
 logdir="$1"
 logfile="$logdir/foo.log"
-outfile="$logdir/out.log"
 rotatefile="$logdir/foo.conf"
 statefile="$logdir/foo.state"
 
@@ -24,12 +22,6 @@ $logfile
         delaycompress
 }
 EOL
-
-cat $rotatefile
-
-#cargo build --example lines
-#/target/debug/examples/lines --no-source $logfile > $outfile &
-#inespid=$!
 
 sleep 0.1
 
@@ -49,28 +41,3 @@ echo "qux" >> $logfile
 sleep 0.1
 
 exit 0
-
-#kill $linespid
-
-out_check="$( cat $outfile )"
-out_expect="$( cat << EOL
-foo
-bar
-baz
-qux
-EOL
-)"
-
-ret=0
-if [[ "$out_check" != "$out_expect" ]]; then
-  ret=1
-fi
-
-rm $statefile
-rm $rotatefile
-rm $outfile
-rm $logfile
-rm $logfile.1 || echo 'rotated log no longer exists'
-rmdir $logdir || ls $logdir
-
-exit $ret
