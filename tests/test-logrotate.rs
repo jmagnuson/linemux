9e80f932e998e1b7c4595890f135f3317cc3446f
Jon Magnuson <jon.magnuson@gmail.com>
2021-09-19 22:37:42 -0500

    nice works

--- tests/test-logrotate.rs
+++ tests/test-logrotate.rs
@@ -9,16 +9,18 @@ use std::time::Duration;
 #[tokio::test]
 #[ignore]
 pub async fn test_logrotate() -> std::io::Result<()> {
-    let line_vals_expected = vec!["foo", "bar", "baz", "quz"];
+    let line_vals_expected = vec!["foo", "bar", "baz", "qux"];
 
     let logdir = tempdir().unwrap();
     let logdir_path = logdir.path();
     let logfile = logdir_path.join("foo.log");
 
+    /*
     let logdir_path_clone = logdir_path.to_owned();
     let thandle = tokio::task::spawn_blocking(move || {
         let _logdir_path = logdir_path_clone;
     });
+    */
 
     // TODO: CARGO_MANIFEST_DIR mabye
     //
@@ -29,7 +31,7 @@ pub async fn test_logrotate() -> std::io::Result<()> {
         .expect("failed to spawn");
 
     // Await until the command completes
-    let status = child.wait().await?;
+    //let status = child.wait().await?;
 
     let mut lines = MuxedLines::new()?;
 
@@ -37,11 +39,13 @@ pub async fn test_logrotate() -> std::io::Result<()> {
 
     let line_vals_fut = lines
         .map(|line| line.unwrap().into_inner().1)
+        .take(4)
         .collect::<Vec<_>>();
 
     let (line_vals, _) = tokio::try_join!(
         time::timeout(Duration::from_millis(5000), line_vals_fut),
-        time::timeout(Duration::from_millis(5000), thandle),
+        //time::timeout(Duration::from_millis(5000), thandle),
+        time::timeout(Duration::from_millis(5000), child.wait()),
     ).unwrap();
 
     assert_eq!(line_vals_expected, line_vals);
